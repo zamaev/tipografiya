1d3cf11ce1cb01af0a9461365d27398cea1aa733
Aydrus Zamaev <aydrus.zamaev@gmail.com>
2022-01-06 00:02:08 +0300

    finish header

---
+++ blocks/header.php
@@ -0,0 +1,49 @@
+<header role="banner">
+    <div class="header__content">
+        <input type="checkbox" id="menu_burger__status">
+        <div class="menu_burger__content">
+            <label class="menu_burger__icon" for="menu_burger__status">
+                <div></div>
+                <div></div>
+                <div></div>
+            </label>
+            <div class="header__info-block">
+                <div class="header__partner">
+                    <figure>
+                        <img src="assets/img/heidelberg.png" alt="HEIDELBERG">
+                        <figcaption>Предприятие-партнёр</figcaption>
+                    </figure>
+                </div>
+                <div class="address">
+                    <img src="assets/img/location.svg" alt="Адрес">
+                    <p>
+                        г. Москва, М Тимирязевская
+                        <br>
+                        ул. Яблочкова, д. 21, корп. 3, Деловой центр «Я21»
+                    </p>
+                </div>
+                <div class="contact">
+                    <a href="mailto:info@ok-press.ru">info@ok-press.ru</a>
+                    <a href="tel:+74956601572">+7 495 660-15-72</a>
+                </div>
+            </div>
+            <div class="menu_burger__list">
+                <ul role="list">
+                    <li><a href="#">Услуги</a></li>
+                    <li><a href="#">Продукция</a></li>
+                    <li><a href="#">О типографии</a></li>
+                    <li><a href="#">Информация</a></li>
+                    <li>
+                        <button type="button" class="header__button btn btn-fill">Перезвоните мне</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <a href="#" class="header__logo">
+            <img src="../assets/img/logo.svg" alt="ok press типография">
+        </a>
+        <div class="header__title header-text">
+            ТИПОГРАФИЯ<br>ОК-ПРЕСС
+        </div>
+    </div>
+</header>
