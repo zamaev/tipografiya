c28b9b87bee5483f9e207d0181822123c80ca473
Aydrus Zamaev <aydrus.zamaev@gmail.com>
2022-01-12 10:27:17 +0300

    bestsellers__wrapper

--- blocks/bestsellers.php
+++ blocks/bestsellers.php
@@ -1,30 +1,32 @@
 <div class="bestsellers">
-    <div class="h1 bestsellers__header">Популярная продукция</div>
-    <div class="bestsellers__products">
-        <a href="#" class="bestsellers__item link-text">
-            <img src="assets/img/p1.png" alt="Буклеты" class="bestsellers__item-image">
-            <div class="bestsellers__item-title">Буклеты</div>
-        </a>
-        <a href="#" class="bestsellers__item link-text">
-            <img src="assets/img/p2.png" alt="Блокноты" class="bestsellers__item-image">
-            <div class="bestsellers__item-title">Блокноты</div>
-        </a>
-        <a href="#" class="bestsellers__item link-text">
-            <img src="assets/img/p3.png" alt="Календари" class="bestsellers__item-image">
-            <div class="bestsellers__item-title">Календари</div>
-        </a>
-        <a href="#" class="bestsellers__item link-text">
-            <img src="assets/img/p4.png" alt="Папки" class="bestsellers__item-image">
-            <div class="bestsellers__item-title">Папки</div>
-        </a>
-        <a href="#" class="bestsellers__item link-text">
-            <img src="assets/img/p5.png" alt="Упаковка" class="bestsellers__item-image">
-            <div class="bestsellers__item-title">Упаковка</div>
-        </a>
-        <a href="#" class="bestsellers__item link-text">
-            <img src="assets/img/p6.png" alt="Лифлеты" class="bestsellers__item-image">
-            <div class="bestsellers__item-title">Лифлеты</div>
-        </a>
+    <div class="bestsellers__wrapper">
+        <div class="h1 bestsellers__header">Популярная продукция</div>
+        <div class="bestsellers__products">
+            <a href="#" class="bestsellers__item link-text">
+                <img src="assets/img/p1.png" alt="Буклеты" class="bestsellers__item-image">
+                <div class="bestsellers__item-title">Буклеты</div>
+            </a>
+            <a href="#" class="bestsellers__item link-text">
+                <img src="assets/img/p2.png" alt="Блокноты" class="bestsellers__item-image">
+                <div class="bestsellers__item-title">Блокноты</div>
+            </a>
+            <a href="#" class="bestsellers__item link-text">
+                <img src="assets/img/p3.png" alt="Календари" class="bestsellers__item-image">
+                <div class="bestsellers__item-title">Календари</div>
+            </a>
+            <a href="#" class="bestsellers__item link-text">
+                <img src="assets/img/p4.png" alt="Папки" class="bestsellers__item-image">
+                <div class="bestsellers__item-title">Папки</div>
+            </a>
+            <a href="#" class="bestsellers__item link-text">
+                <img src="assets/img/p5.png" alt="Упаковка" class="bestsellers__item-image">
+                <div class="bestsellers__item-title">Упаковка</div>
+            </a>
+            <a href="#" class="bestsellers__item link-text">
+                <img src="assets/img/p6.png" alt="Лифлеты" class="bestsellers__item-image">
+                <div class="bestsellers__item-title">Лифлеты</div>
+            </a>
+        </div>
+        <button type="button" class="btn btn-outline bestsellers__buttom">ВСЯ ПРОДУКЦИЯ</button>
     </div>
-    <button type="button" class="btn btn-outline bestsellers__buttom">ВСЯ ПРОДУКЦИЯ</button>
 </div>
